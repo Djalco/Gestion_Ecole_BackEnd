3b66d397d3747b8cc8b51e82eb9f446c0e1f0b88
Djiki Alexia <djikialexia@gmail.com>
2026-02-07 00:11:49 +0100

    BD

--- src/main/java/ecole/gestionecole/GestionecoleApplication.java
+++ src/main/java/ecole/gestionecole/GestionecoleApplication.java
@@ -2,13 +2,17 @@ package ecole.gestionecole;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class GestionecoleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionecoleApplication.class, args);
 	}
-	
+
 
 }

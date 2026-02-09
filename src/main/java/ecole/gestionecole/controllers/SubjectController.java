0f04dc592f382336be3264d89d41ae79befc47e7
Djiki Alexia <djikialexia@gmail.com>
2026-02-09 14:48:48 +0100

    ..

--- src/main/java/ecole/gestionecole/controllers/SubjectController.java
+++ src/main/java/ecole/gestionecole/controllers/SubjectController.java
@@ -2,6 +2,7 @@ package ecole.gestionecole.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/subjects")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class SubjectController {
 
     private final SubjectService subjectService;
